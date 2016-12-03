95d20362ae1ae083e51b4768b4cc625caa67fc4e
Soufiane  Ghzal <sghzal@gmail.com>
2016-12-03 23:28:29 +0100

    sample app disable dov env parsing

--- test/application-sample/bootstrap.php
+++ test/application-sample/bootstrap.php
@@ -10,12 +10,19 @@ use Dotenv\Dotenv;
 $composer = include __DIR__ . '/../../vendor/autoload.php';
 $GLOBALS['composer'] = $composer;
 
-$env = new Dotenv(__DIR__);
-$env->load();
 
-$options = [
-    'dev' => getenv('DEV') === 'true',
-    'env' => getenv('ENV') ?? 'production'
-];
+if (file_exists(__DIR__ . '/.env')) {
+    $env = new Dotenv(__DIR__);
+    $env->load();
+    $options = [
+        'dev' => getenv('DEV') === 'true',
+        'env' => getenv('ENV') ?? 'production'
+    ];
+} else {
+    $options = [
+        'dev' => false,
+        'env' => 'production'
+    ];
+}
 
 return new Application(__DIR__ . '/app', $composer, $options);
